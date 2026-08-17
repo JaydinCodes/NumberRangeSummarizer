eb0564147bfea59396d0a615279b7f77475d3e1d
Jaydin Morrison <4260354@myuwc.ac.za>
2026-08-17 13:39:16 +0200

    implemented the collect method

--- src/main/java/numberrangesummarizer/NumberRangeSummarizerImpl.java
+++ src/main/java/numberrangesummarizer/NumberRangeSummarizerImpl.java
@@ -1,13 +1,22 @@
 package numberrangesummarizer;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class NumberRangeSummarizerImpl implements NumberRangeSummarizer{
     @Override
     public Collection<Integer> collect(String input) {
-        return Collections.emptyList();
+        if (input == null || input.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return Stream.of(input.split(",", -1))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
     @Override

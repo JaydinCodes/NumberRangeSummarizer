a940bc16a4f03d69c0b9ee6506194c7f7e2cc7ac
Jaydin Morrison <4260354@myuwc.ac.za>
2026-08-17 12:55:50 +0200

    implemeneted test

--- src/main/java/numberrangesummarizer/NumberRangeSummarizerImpl.java
+++ src/main/java/numberrangesummarizer/NumberRangeSummarizerImpl.java
@@ -3,6 +3,7 @@ package numberrangesummarizer;
 import java.util.Collection;
 import java.util.Collections;
 
+
 public class NumberRangeSummarizerImpl implements NumberRangeSummarizer{
     @Override
     public Collection<Integer> collect(String input) {
--- src/test/java/numberrangesummarizer/NumberRangeSummarizerImplTest.java
+++ src/test/java/numberrangesummarizer/NumberRangeSummarizerImplTest.java
@@ -2,14 +2,30 @@ package numberrangesummarizer;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NumberRangeSummarizerImplTest {
 
+    private final NumberRangeSummarizer summarizer = new NumberRangeSummarizerImpl();
     @Test
-    void canCreateSummarizer(){
-        NumberRangeSummarizer summarizer = new NumberRangeSummarizerImpl();
+    void shouldCollectCommaDelimitedNumbers(){
+        Collection<Integer> result = summarizer.collect("1,3,6,7,8,12,13,14,15,21,22,23,24,31");
 
-        assertNotNull(summarizer);
+        assertEquals(
+                Arrays.asList(1,3,6,7,8,12,13,14,15,21,22,23,24,31), result
+        );
+    }
+
+    @Test
+    void shouldSummarizeSequentialNumbersIntoRanges(){
+        Collection<Integer> input = Arrays.asList(1,3,6,7,8,12,13,14,15,21,22,23,24,31);
+
+        String result = summarizer.summarizeCollection(input);
+
+        assertEquals("1, 3, 6-8, 12-15, 21-24, 31", result);
     }
 }
